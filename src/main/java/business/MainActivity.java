f4ba573f142b3dc934cd2523116de4772b970904
Eugenio Moro <moro.eugenio@yahoo.it>
2016-11-22 00:21:09 +0100

    Flow control for strings

--- src/main/java/business/MainActivity.java
+++ src/main/java/business/MainActivity.java
@@ -1,18 +1,42 @@
 package business;
 
+import model.Feed;
+import model.FeedMessage;
+import serialCommRXTX.FlowControl;
+import serialCommRXTX.SerialComm;
+
 public class MainActivity {
 
 	public static void main(String[] args) throws Exception {
-//
-//		RSSFeedParser parser = new RSSFeedParser(
-//				"http://www.ansa.it/sito/notizie/topnews/topnews_rss.xml");
-//		Feed feed = parser.readFeed();
-//		System.out.println(feed);
-//		feed.getMessages().size();
-//		for (FeedMessage message : feed.getMessages()) {
-//			System.out.println(message);		}
-//	}
 
+		RSSFeedParser parser = new RSSFeedParser(
+				"http://www.ansa.it/sito/notizie/topnews/topnews_rss.xml");
+		Feed feed = parser.readFeed();
+		System.out.println(feed);
+		feed.getMessages().size();
+		String s="";
+		for (FeedMessage message : feed.getMessages()) {
+			s=s+"||||"+message.getTitle();		
+			}
+		
+		System.out.println(s);
+
+		//testing serial comm
+		
+		//init
+//		SerialComm.getInstance().initialize();
+//		
+//		String s="01234567890123456789012345678901234567890123456789fiojrgoifjq34fpij23opdij409rvjefpoiwednopin";
+//		s.charAt(0);
+//		
+//		SerialComm.getInstance().writeBytes(s.getBytes("ASCII"));
+		SerialComm.getInstance();
+		Thread.sleep(5000);
+		//FlowControl.getInstance().appendString("");
+		FlowControl.getInstance().appendString(s);
+
+
+		
 
 		
 }
---
+++ src/main/java/serialCommRXTX/FlowControl.java
@@ -0,0 +1,129 @@
+package serialCommRXTX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+/*
+ * This class acts as flow control for sending strings with length>63
+ */
+
+public class FlowControl implements SerialPortEventListener{
+	
+	private static FlowControl instance;
+	private Boolean isWorking=false;
+	private Vector<String> stringVector = new Vector<String>();
+	private Lock lock = new ReentrantLock();
+	private Condition newAppend = lock.newCondition();
+	private Condition write = lock.newCondition();
+	private Boolean canTransfer=true;
+	private Boolean tooLong=false;
+	private int tooLongCount=0;
+	private Thread worker = new Thread(new Runnable() {
+		
+		@Override
+		public void run() {
+			while(true){
+				processStringsTask();
+			}
+		}
+	});
+	
+
+	public static FlowControl getInstance(){
+		if(instance==null){
+			instance=new FlowControl();
+		}
+		return instance;
+	}
+	
+	@Override
+	public void serialEvent(SerialPortEvent se) {
+		if (se.getEventType() == SerialPortEvent.DATA_AVAILABLE){
+			SerialPort sp = (SerialPort) se.getSource();
+			lock.lock();
+			try {
+				BufferedReader input = new BufferedReader(new InputStreamReader(sp.getInputStream()));
+				if(input.readLine().equals("c")){
+					//System.out.println("diomerda");
+					canTransfer=true;
+					write.signalAll();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {lock.unlock();}
+			
+		}
+		
+	}
+	
+	public void appendString(String s){  //recursive
+		lock.lock();
+		try{
+			if(s.length()>60){
+				canTransfer=false;
+				tooLong=true;
+				tooLongCount++;
+				this.stringVector.addElement(s.substring(0, 60));
+				appendString(s.substring(60));
+			} else {
+				this.stringVector.addElement(s);
+			}
+			newAppend.signal();
+		} finally {
+			lock.unlock();
+			startWorker();
+		}
+	}
+	
+	private void processStringsTask(){
+		lock.lock();
+		try {
+			while(stringVector.isEmpty()){
+				newAppend.await();
+			}
+			sendString(stringVector.remove(0));
+			if(tooLong){
+				canTransfer=false;
+				if(tooLongCount==0){
+					tooLong=false;
+					canTransfer=true;
+				} else {
+					tooLongCount--;
+				}
+				while(tooLong && !canTransfer){
+				write.await();
+			}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+	
+	public void startWorker(){
+		if (!isWorking){
+			this.worker.start();
+			isWorking=true;
+		}
+	}
+	
+	
+	private void sendString(String s){
+		System.out.println("Sending: "+s);
+		SerialComm.getInstance().writeString(s);
+	}
+	
+
+}
